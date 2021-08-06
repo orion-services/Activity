96c8797da747079d143e05c5addb6fd365cfe2ec
Nikolas Freitas <nikolasbvf@gmail.com>
2021-08-06 20:54:27 -0300

    #14 clean not used endpoints

--- src/main/java/dev/orion/api/endpoint/v1/ActivityEndpointV1.java
+++ src/main/java/dev/orion/api/endpoint/v1/ActivityEndpointV1.java
@@ -2,7 +2,6 @@ package dev.orion.api.endpoint.v1;
 
 import dev.orion.data.entity.Activity;
 
-import javax.inject.Inject;
 import javax.transaction.Transactional;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
