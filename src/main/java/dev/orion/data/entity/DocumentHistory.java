f969b0bd1547b97578f26146e161eab3b4fdd15a
Nikolas Freitas <nikolasbvf@gmail.com>
2021-08-11 22:43:56 -0300

    #22 fix lint

--- src/main/java/dev/orion/data/entity/DocumentHistory.java
+++ src/main/java/dev/orion/data/entity/DocumentHistory.java
@@ -4,7 +4,6 @@ import io.quarkus.hibernate.orm.panache.PanacheEntity;
 
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
 
 /**
  * WIP: HISTORY NOT IMPLEMENTED YET
