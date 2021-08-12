96e0cfa0ce620d7470aa2736f0ab04b3d92b30c2
Nikolas Freitas <nikolasbvf@gmail.com>
2021-08-12 01:49:12 -0300

    #18 Add document entity and documentHistory entity relationship

--- src/main/java/dev/orion/services/ActivityServiceImpl.java
+++ src/main/java/dev/orion/services/ActivityServiceImpl.java
@@ -1,6 +1,43 @@
 package dev.orion.services;
 
+import dev.orion.client.UserClient;
+import dev.orion.data.entity.Activity;
+import dev.orion.data.entity.User;
 import dev.orion.services.interfaces.ActivityService;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
 
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import java.util.UUID;
+
+@ApplicationScoped
 public class ActivityServiceImpl implements ActivityService {
+    @Inject
+    @RestClient
+    UserClient userClient;
+
+    @Override
+    public UUID createActivity(String userToken) {
+        return null;
+    }
+
+    @Override
+    public Activity addUserInActivity(UUID activityUuid, String userToken) {
+        return null;
+    }
+
+    @Override
+    public Activity removeUserFromActivity(UUID activityUuid, String userToken) {
+        return null;
+    }
+
+    @Override
+    public Activity endActivity(UUID activityUuid) {
+        return null;
+    }
+
+    @Override
+    public Boolean canUserEditDocument(UUID activityUuid, User user) {
+        return null;
+    }
 }
