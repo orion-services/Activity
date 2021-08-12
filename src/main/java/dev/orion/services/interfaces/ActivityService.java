d0355dbb5fe73dcf730bf255c018be8666320dab
Nikolas Freitas <nikolasbvf@gmail.com>
2021-08-12 01:52:21 -0300

    #18 Add ActivityService more methods

--- src/main/java/dev/orion/services/interfaces/ActivityService.java
+++ src/main/java/dev/orion/services/interfaces/ActivityService.java
@@ -1,4 +1,14 @@
 package dev.orion.services.interfaces;
 
+import dev.orion.data.entity.Activity;
+import dev.orion.data.entity.User;
+
+import java.util.UUID;
+
 public interface ActivityService {
+    public UUID createActivity(String userToken);
+    public Activity addUserInActivity(UUID activityUuid, String userToken);
+    public Activity removeUserFromActivity(UUID activityUuid, String userToken);
+    public Activity endActivity(UUID activityUuid);
+    public Boolean canUserEditDocument(UUID activityUuid, User user);
 }
