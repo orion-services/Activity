064cb4fd04487000d545eb460df7a068382d5e55
Nikolas Freitas <nikolasbvf@gmail.com>
2021-08-13 02:45:08 -0300

    #18 Adjust user client route

--- src/main/java/dev/orion/client/UserClient.java
+++ src/main/java/dev/orion/client/UserClient.java
@@ -1,6 +1,7 @@
 package dev.orion.client;
 
 import dev.orion.client.dto.UserClientDto;
+import org.eclipse.microprofile.faulttolerance.Timeout;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 import javax.ws.rs.GET;
@@ -9,12 +10,13 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/v1/user")
+@Path("/v1/users")
 @RegisterRestClient(configKey = "api.user-service.client")
 public interface UserClient {
 
     @GET
     @Path("/{externalId}")
     @Produces(MediaType.APPLICATION_JSON)
+    @Timeout(1000)
     UserClientDto getUserByExternalId(@PathParam("externalId") String externalId);
 }
