047ff10174c42e2c942d37f1e4fc71c9d04d6da5
Nikolas Freitas <nikolasbvf@gmail.com>
2021-08-16 23:14:27 -0300

    #27 Fix test renamed

--- src/test/java/dev/orion/NativeActivityResourceTest.java
+++ src/test/java/dev/orion/NativeActivityResourceTest.java
@@ -3,6 +3,6 @@ package dev.orion;
 import io.quarkus.test.junit.NativeImageTest;
 
 @NativeImageTest
-public class NativeActivityApiTest extends ActivityApiTest {
+public class NativeActivityResourceTest extends ActivityResourceTest {
     // Execute the same tests but in native mode.
 }
