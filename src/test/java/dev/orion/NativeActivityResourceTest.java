09352cc04089226fd540f7ddc2b668c9895a47cb
Nikolas Freitas <nikolasbvf@gmail.com>
2021-08-16 23:09:29 -0300

    #27 Rename tests of activity

--- src/test/java/dev/orion/NativeActivityResourceTest.java
+++ src/test/java/dev/orion/NativeActivityResourceTest.java
@@ -3,6 +3,6 @@ package dev.orion;
 import io.quarkus.test.junit.NativeImageTest;
 
 @NativeImageTest
-public class NativeActivityResourceTest extends ActivityResourceTest{
+public class NativeActivityApiTest extends ActivityApiTest {
     // Execute the same tests but in native mode.
 }
