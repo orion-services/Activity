d8d723ac7a6a320a700fba02abe0a2e982260c82
Nikolas Freitas <nikolasbvf@gmail.com>
2022-03-22 01:48:01 -0300

    Add Data faker library and create activity entity test

---
+++ src/test/java/dev/orion/fixture/UserFixture.java
@@ -0,0 +1,33 @@
+package dev.orion.fixture;
+
+import dev.orion.client.dto.UserClientDto;
+import dev.orion.data.entity.User;
+import dev.orion.services.dto.UserEnhancedWithExternalDataDto;
+import net.datafaker.Faker;
+
+import java.util.UUID;
+
+final public class UserFixture {
+    static public User generateUser() {
+        User user = new User();
+        user.id = Faker.instance().number().randomNumber();
+        user.externalId = UUID.randomUUID().toString();
+
+        return user;
+    }
+
+    static public UserClientDto generateClientDto() {
+        UserClientDto userClientDto = new UserClientDto();
+        userClientDto.isActive = true;
+        userClientDto.name = Faker.instance().funnyName().name();
+
+        return  userClientDto;
+    }
+
+    static public UserEnhancedWithExternalDataDto generateUserEnhancedWithExternalDataDto() {
+        var user = generateUser();
+        var userClientDto = generateClientDto();
+
+        return new UserEnhancedWithExternalDataDto(user, userClientDto);
+    }
+}
