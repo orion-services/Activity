6e126208ebb0edad22dd1810cba4fe89a77ee2b4
Nikolas B V Freitas <nikolas.freitas@sap.com>
2022-05-15 22:52:29 -0300

    Create add user to a group feature

--- src/main/java/dev/orion/services/GroupServiceImpl.java
+++ src/main/java/dev/orion/services/GroupServiceImpl.java
@@ -1,17 +1,25 @@
 package dev.orion.services;
 
+import dev.orion.commom.exceptions.UserInvalidOperationException;
 import dev.orion.entity.Activity;
 import dev.orion.entity.Group;
 import dev.orion.entity.User;
 import dev.orion.services.interfaces.GroupService;
+import io.quarkus.arc.log.LoggerName;
 import lombok.val;
+import org.jboss.logging.Logger;
 import org.jboss.resteasy.spi.NotImplementedYetException;
 
 import javax.enterprise.context.ApplicationScoped;
+import java.text.MessageFormat;
 import java.util.List;
+import java.util.Objects;
 
 @ApplicationScoped
 public class GroupServiceImpl implements GroupService {
+
+    @LoggerName("GroupServiceImpl")
+    Logger logger;
 
     @Override
     public Group createGroup(Activity activity) {
@@ -30,7 +38,42 @@ public class GroupServiceImpl implements GroupService {
 
     @Override
     public void addUserToGroup(Activity activity, Group group, User user) {
+        validateUserInsertionOnGroup(activity, group, user);
+
         group.addParticipant(user);
+        user.setGroup(group);
+    }
+
+    private void validateUserInsertionOnGroup(Activity activity, Group group, User user) {
+        notInAnotherGroupValidation(user);
+        groupCapacityValidation(group, user);
+        userBelongsToSameActivityOfGroup(group, user);
+    }
+
+    private void notInAnotherGroupValidation(User user) {
+        if (!Objects.isNull(user.getGroup())) {
+            val errorMessage = MessageFormat.format("The user {0} is already on another group", user.id);
+            logger.error(errorMessage);
+            throw new UserInvalidOperationException(errorMessage);
+        }
+    }
+
+    private void groupCapacityValidation(Group group, User user) {
+        if (group.getCapacity() <= group.getParticipants().size() && !group.getParticipants().contains(user)) {
+            val errorMessage = MessageFormat.format("The user {0} can't be placed on group {1} because its is full", user.id,group.id);
+            logger.error(errorMessage);
+            throw new UserInvalidOperationException(errorMessage);
+        }
+    }
+
+    private void userBelongsToSameActivityOfGroup(Group group, User user) {
+        if (!group.getActivityOwner().userList.contains(user)) {
+            val errorMessage = MessageFormat
+                    .format("The user {0} can't be placed on group {1} because it not belongs to activity {2}",
+                            user.id, group.id, group.getActivityOwner().uuid);
+            logger.error(errorMessage);
+            throw new UserInvalidOperationException(errorMessage);
+        }
     }
 
     @Override
--- src/test/java/dev/orion/services/GroupServiceTest.java
+++ src/test/java/dev/orion/services/GroupServiceTest.java
@@ -1,5 +1,6 @@
 package dev.orion.services;
 
+import dev.orion.commom.exceptions.UserInvalidOperationException;
 import dev.orion.entity.Activity;
 import dev.orion.entity.Group;
 import dev.orion.entity.User;
@@ -8,10 +9,7 @@ import dev.orion.services.interfaces.GroupService;
 import io.quarkus.test.junit.QuarkusTest;
 import lombok.val;
 import org.jboss.resteasy.spi.NotImplementedYetException;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.BDDMockito;
 import org.mockito.Mockito;
@@ -19,6 +17,7 @@ import org.mockito.MockitoAnnotations;
 
 import javax.inject.Inject;
 import javax.transaction.Transactional;
+import java.text.MessageFormat;
 
 @QuarkusTest
 @Transactional
@@ -41,65 +40,125 @@ public class GroupServiceTest {
         activity.userList.add(mainUser);
 
         val group = groupService.createGroup(activity);
-        activity.groups.add(group);
-        mainUser.setGroup(group);
         val spyGroup = Mockito.spy(group);
+
+        activity.groups.add(group);
 
         groupService.addUserToGroup(activity, spyGroup, mainUser);
         Assertions.assertEquals(group.getParticipants().size(), 1);
+        Assertions.assertEquals(mainUser.getGroup(), spyGroup);
         BDDMockito.then(spyGroup).should().addParticipant(mainUser);
     }
 
     @Test
     @DisplayName("Should throw exception when try to add an user that is already in another group")
     public void testAddUserInMoreThanOneGroup() {
-        throw new NotImplementedYetException();
+        val activity = new Activity();
+        val mainUser = UserFixture.generateUser();
+        activity.userList.add(mainUser);
+
+        val groupOrigin = groupService.createGroup(activity);
+        val groupDestination = groupService.createGroup(activity);
+
+        activity.groups.add(groupOrigin);
+        activity.groups.add(groupDestination);
+
+        groupService.addUserToGroup(activity, groupOrigin, mainUser);
+        String errorMessage = Assertions.assertThrows(UserInvalidOperationException.class, () -> {
+            groupService.addUserToGroup(activity, groupDestination, mainUser);
+        }).getMessage();
+        Assertions.assertEquals(MessageFormat.format("The user {0} is already on another group", mainUser.id), errorMessage);
+        Assertions.assertNotEquals(mainUser.getGroup(), groupDestination);
     }
 
     @Test
     @DisplayName("Should not let add participant if group is full")
     public void testAddToFullGroupValidation() {
-        throw new NotImplementedYetException();
+        val activity = new Activity();
+        val mainUser = UserFixture.generateUser();
+        activity.userList.add(mainUser);
+
+        val group = groupService.createGroup(activity);
+        groupService.addUserToGroup(activity, group, mainUser);
+        val secondUser = UserFixture.generateUser();
+
+        activity.groups.add(group);
+
+        String message = Assertions.assertThrows(UserInvalidOperationException.class, () -> {
+            groupService.addUserToGroup(activity, group, secondUser);
+        }).getMessage();
+
+        String expectedErrorMessage = MessageFormat.format("The user {0} can't be placed on group {1} because its is full", secondUser.id, group.id);
+        Assertions.assertEquals(expectedErrorMessage, message);
     }
 
     @Test
     @DisplayName("Should not let add participant if they not belongs to same activity that owns group")
     public void testAddUserThatNotBelongToActivityOwnerValidation() {
-        throw new NotImplementedYetException();
+        val activity = new Activity();
+        val mainUser = UserFixture.generateUser();
+
+        val group = groupService.createGroup(activity);
+        group.setCapacity(2);
+
+        activity.groups.add(group);
+
+        String message = Assertions.assertThrows(UserInvalidOperationException.class, () -> {
+            groupService.addUserToGroup(activity, group, mainUser);
+        }).getMessage();
+        String expectedErrorMessage = MessageFormat
+                .format("The user {0} can't be placed on group {1} because it not belongs to activity {2}",
+                        mainUser.id, group.id, activity.uuid);
+        Assertions.assertEquals(expectedErrorMessage, message);
     }
 
     @Test
     @DisplayName("Should transfer an user between groups")
+    @Disabled
     public void testTransferUserBetweenGroups() {
         throw new NotImplementedYetException();
     }
 
     @Test
     @DisplayName("Should not transfer an user if it's not present in activity")
+    @Disabled
     public void testTransferUserNotBelongingToActivityBetweenGroupsValidation() {
         throw new NotImplementedYetException();
     }
 
-//    Capacity scenarios
+    //    Capacity scenarios
     @Test
     @DisplayName("Should create a group with capacity same number of activity participants")
-    public void testGroupCapacityAsSameActivityParticipantNumber() { throw new NotImplementedYetException();}
+    @Disabled
+    public void testGroupCapacityAsSameActivityParticipantNumber() {
+        throw new NotImplementedYetException();
+    }
 
     @Test
     @DisplayName("Should create a group with same number of provided user list")
-    public void testGroupCapacityAsSameProvidedUserList() { throw new NotImplementedYetException();}
+    @Disabled
+    public void testGroupCapacityAsSameProvidedUserList() {
+        throw new NotImplementedYetException();
+    }
 
     @Test
     @DisplayName("Should change capacity of group")
-    public void testChangeGroupCapacity() { throw new NotImplementedYetException();}
+    @Disabled
+    public void testChangeGroupCapacity() {
+        throw new NotImplementedYetException();
+    }
 
     @Test
     @DisplayName("Should not change capacity when the new quantity is greater than number of participants")
-    public void testChangeGroupCapacityBelowParticipantsNumberValidation() { throw new NotImplementedYetException();}
+    @Disabled
+    public void testChangeGroupCapacityBelowParticipantsNumberValidation() {
+        throw new NotImplementedYetException();
+    }
 
     //    Document scenarios
     @Test
     @DisplayName("Should add document and users whenever is possible")
+    @Disabled
     public void testAddDocumentAndUsers() {
         throw new NotImplementedYetException();
     }
