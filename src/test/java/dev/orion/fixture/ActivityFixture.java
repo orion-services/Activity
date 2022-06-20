f8beae16da1d1e5137de30f5237c69ceb078dde2
Nikolas Freitas <nikolas.freitas@sap.com>
2022-06-19 21:41:57 -0300

    Fix workspace test

---
+++ src/test/java/dev/orion/fixture/ActivityFixture.java
@@ -0,0 +1,31 @@
+package dev.orion.fixture;
+
+import dev.orion.commom.constant.ActivityStages;
+import dev.orion.commom.constant.CircularStepFlowDirectionTypes;
+import dev.orion.entity.Activity;
+import dev.orion.entity.Step;
+import dev.orion.entity.User;
+import dev.orion.entity.step_type.CircleOfWriters;
+import dev.orion.util.setup.WorkflowStarter;
+import lombok.val;
+
+import java.util.List;
+import java.util.UUID;
+
+public class ActivityFixture {
+    public static Activity generateActivity(User user) {
+        val activityUuid = UUID.randomUUID();
+        Activity activity = new Activity();
+        activity.uuid = activityUuid;
+        activity.isActive = true;
+        activity.setCreator(user);
+
+        val stepList = List.of(new Step[]{new CircleOfWriters(CircularStepFlowDirectionTypes.FROM_BEGIN_TO_END)});
+        val stage = WorkflowFixture.generateStage(ActivityStages.DURING, stepList);
+        val workflow = WorkflowFixture.generateWorkflow(List.of(stage));
+        workflow.setName(WorkflowStarter.GENERIC_WORKFLOW_NAME);
+        activity.setWorkflow(workflow);
+
+        return activity;
+    }
+}
